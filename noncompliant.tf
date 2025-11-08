444316f33f883e2b9954e97da4ff7ac0064b3a28
Sneha0615 <113693337+Sneha0615@users.noreply.github.com>
2025-11-08 15:05:07 +0530

    sgnoncompliant

---
+++ noncompliant.tf
@@ -0,0 +1,9 @@
+resource "aws_security_group" "violating" {
+  name = "violating-sg"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
