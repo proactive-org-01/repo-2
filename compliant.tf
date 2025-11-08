0c4ec099262627d6afae087f5efa3cbf905d0dcd
Sneha0615 <113693337+Sneha0615@users.noreply.github.com>
2025-11-08 15:03:30 +0530

    sgcompliant

---
+++ compliant.tf
@@ -0,0 +1,9 @@
+resource "aws_security_group" "restricted" {
+  name = "restricted-sg"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+}
